9a4a2cd8f8a51b13f1e93454cfa87b1ee2f7beab
jinareum <jinareum.76@gmail.com>
2021-08-02 23:43:55 +0900

    promise

--- dream-coding/basic/callback.js
+++ dream-coding/basic/callback.js
@@ -3,4 +3,61 @@
 // javascript is synchronous. by ordger => in order
 // hoisting : var , function 선언을 상위로 올림
 
-//1. 동기와 비동기
+
+//class object 생성
+class UserStorage {
+    loginUser(id, password) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (
+                    (id === 'ellie' && password === 'dream') ||
+                    (id === 'coder' && password === 'academy')
+                ) {
+                    resolve(id);
+                } else {
+                    reject(new Error('not found'));
+                }
+            }, 2000);
+        });
+    }
+
+    getRoles(user) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (user === 'ellie') {
+                    resolve({ name: 'ellie', role: 'admin' });
+                } else {
+                    reject(new Error('no access'));
+                }
+            }, 1000);
+        });
+    }
+
+    // Homework Answer 🚀
+    async getUserWithRole(user, password) {
+        const id = await this.loginUser(user, password);
+        const role = await this.getRoles(id);
+        return role;
+    }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const pw = prompt('enter your pwd');
+userStorage.loginUser(
+    id,pw,
+    user=>{
+        userStorage.getRoles(
+            user,
+            userWithRole =>{
+                alert(
+                    `hello ${userWithRole.name}, you have a ${userWithRole.role} role`
+                )
+            },
+            error=>{
+                console.log(error);
+            }
+        );
+    },
+    error=>{console.log(error)}
+)
---
+++ dream-coding/basic/promise.js
@@ -0,0 +1,25 @@
+//js 비동기를 간편하게 도와주는 오브젝트
+// promise is a javascript for asynchronous operation.
+// 2 points
+// 1. state --> 상태
+// 2. producer (정보 제공자) / consumer(정보 소비자)
+
+//state : pending -> full filled or rejected
+//Producer vs Consumer
+
+// 1. producer part -> Promise 만들기
+// when new Promise is created, the excutor runs automatically.(!)
+const promise = new Promise((resolve,reject) => {
+    //doing some heavy work() 시간이 많이 걸리는 일은 프로미스를 이용해서 비동기적으로 만드는것이 좋다.
+    // network / read files
+    console.log('doing something');
+    //promise를 만든 순 바로 함수가 실행된다 즉슨, 프로미스 안에 네트워크통신을 작성했다면 프로미스 만들어진 그 순간 네트워크 통신을 바로 수행한다.
+    setTimeout(() =>{
+        resolve('ellie')
+    },2000)
+});
+
+// 2. consumers : then, catch, finally 를 이용해서 값을 받아올 수 있음
+promise.then((value) => {
+    console.log(value);
+});
