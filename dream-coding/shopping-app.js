3616d7db983d8f03cfcd736f6b36afe6f914c199
arjin <arjin@cndfactory.com>
2021-10-07 18:51:45 +0900

    중간커밋

---
+++ dream-coding/shopping-app.js
@@ -0,0 +1,11 @@
+const item = document.querySelector('.items');
+const input = document.querySelector('.footer-input');
+const addBtn = document.querySelector('.footer-button');
+
+//add버튼 눌렀을때 이벤트 처리
+function onAdd(){
+    // 1. 사용자가 입력한 텍스트를 받아오기
+    // 2. 받아온 텍스트로 새로운 아이템을 만든다. ( 텍스트 + 삭제버튼)
+    // 3. items 컨테이너 안에 새로만든 아이템을 추가한다.
+    // 4. 인풋을 초기화한다.
+}
