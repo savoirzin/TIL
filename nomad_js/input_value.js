d2b24a5d25a341bc30de4983197dac4a4a5f9852
jinareum <jinareum.76@gmail.com>
2021-12-13 00:36:06 +0900

    input value - nomad corders

---
+++ nomad_js/input_value.js
@@ -0,0 +1,10 @@
+const loginInput = document.querySelector("#login-form input");
+const loginButton = loginForm.querySelector("#login-form button");
+
+
+function onLoginBtnClick(){
+    const username = loginInput.value;
+
+
+}
+loginButton.addEventListener("submit",onLoginBtnClick);
